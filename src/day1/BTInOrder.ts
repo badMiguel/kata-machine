aad0693c8df4fcd4185d9d6e7264f5925a833e11
MiguelBad <badayosbiboy@gmail.com>
2025-03-23 22:02:25 +0930

    bt inorder

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function dfs(curr: BinaryNode<number> | null, path: number[]) {
+    if (!curr) {
+        return;
+    }
 
+    dfs(curr.left, path);
+    path.push(curr.value);
+    dfs(curr.right, path);
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    dfs(head, path);
+    return path;
 }
