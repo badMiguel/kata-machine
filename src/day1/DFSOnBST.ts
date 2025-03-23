0cf5002c32dbb32a4a8d595b68c90604f2ac6ec1
miguel <badayosbiboy@gmail.com>
2025-03-24 07:39:53 +0930

    search bst using dfs

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,22 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function walk(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (!curr) {
+        return false;
+    }
 
+    if (curr.value === needle) {
+        return true;
+    }
+
+    if (walk(curr.left, needle)) {
+        return true;
+    }
+    if (walk(curr.right, needle)) {
+        return true;
+    }
+
+    return false;
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return walk(head, needle);
 }
