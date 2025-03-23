41ccd1a176fed654d5341e90f813cad6f2afbd46
MiguelBad <badayosbiboy@gmail.com>
2025-03-23 22:05:57 +0930

    bt post order

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,15 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function dfs(curr: BinaryNode<number> | null, path: number[]) {
+    if (!curr) {
+        return;
+    }
 
+    dfs(curr.left, path);
+    dfs(curr.right, path);
+    path.push(curr.value);
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    dfs(head, path);
+    return path;
 }
