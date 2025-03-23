c3ccea2dbfbceda46eb62611421402578ab8e4f4
MiguelBad <badayosbiboy@gmail.com>
2025-03-23 22:12:48 +0930

    bt bfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,26 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: BinaryNode<number>[] = [];
+    queue.push(head);
 
+    while (queue.length > 0) {
+        const curr = queue.shift();
+
+        if (!curr?.value) {
+            continue;
+        }
+
+        if (curr.value === needle) {
+            return true;
+        }
+
+        if (curr.left) {
+            queue.push(curr.left);
+        }
+
+        if (curr?.right) {
+            queue.push(curr.right);
+        }
+    }
+
+    return false;
 }
