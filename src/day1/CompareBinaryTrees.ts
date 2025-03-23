8427ce1ae4ca804075a6b05ddf6b498d435c475e
miguel <badayosbiboy@gmail.com>
2025-03-24 07:40:11 +0930

    compare binary tree

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,19 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+function dfs(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (!a && !b) {
+        return true;
+    }
 
+    if (!a || !b ) {
+        return false;
+    }
+
+    if (a?.value !== b?.value) {
+        return false;
+    }
+
+    return dfs(a.left, b.left) && dfs(a.right, b.right);
+}
+
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    return dfs(a, b);
 }
